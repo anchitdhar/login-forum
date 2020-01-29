d34d9d20c9207e7b3c4cba3518861bc72a723a2b
anchitdhar <anchitdhar@yahoo.com>
2020-01-30 03:31:23 +0530

    thirdcommit

--- myapp/views.py
+++ myapp/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.forms import UserCreationForm
-
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def indexView(request):
     return render(request,'index.html')
-
+    
+@login_required
 def dashboardView(request):
     return render(request,'dashboard.html')
 
